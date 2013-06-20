b6aa17ca0ea06f2ab0ed39a37ccadaacfa19d1f5
Samuel Mestanza <samuel.mestanza@hotmail.com>
2013-06-20 10:49:07 -0500

    iniciando proyecto con Hibernate...

---
+++ src/hibernatenormal/HibernateNormal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hibernatenormal;
+
+/**
+ *
+ * @author Samuel
+ */
+public class HibernateNormal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
